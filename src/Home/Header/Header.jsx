953b67791b26e7f0e13d9e2228bc3382e2aff968
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2023-08-24 16:18:48 -0300

    Task/header (#2)
    * change language and add google fonts
    
    * initial development of header component
    
    * import header component to app
    
    * add class, styles and effects to navbar links

---
+++ src/Home/Header/Header.jsx
@@ -0,0 +1,137 @@
+import './Header.css';
+import logo from '../../assets/react.svg';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import {
+  Container,
+  Box,
+  List,
+  ListItem,
+  Drawer,
+  Divider,
+  IconButton,
+} from '@mui/material';
+import { Menu, Close } from '@mui/icons-material';
+
+const headers = {
+  Authorization: 'Bearer ' + import.meta.env.VITE_APP_API_TOKEN,
+};
+// const loginData = {
+//   identifier: 'guilhermerl.dev@gmail.com',
+//   password: 'Vg7gzkXf6y!kqDb'
+// };
+
+const Header = () => {
+  const [pages, setPages] = useState([]);
+  const [state, setState] = useState({
+    right: false,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(
+        'http://localhost:1337/api/paginas?fields[0]=menuTitle&fields[1]=slug&fields[2]=homeButtonLink&fields[3]=homeTab',
+        {
+          headers,
+        },
+      );
+      const res = await data.json();
+      setPages(res.data);
+    };
+
+    fetchData();
+  }, []);
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const getLinkNavbar = (attributes, responsive) => {
+    const link = attributes.homeButtonLink;
+    const classNavbar = responsive
+      ? 'responsive-navbar-link'
+      : 'desktop-navbar-link';
+
+    if (link) {
+      const target = attributes.homeTab ? '_blank' : '_self';
+
+      return (
+        <a href={link} target={target} className={classNavbar}>
+          {attributes.menuTitle}
+        </a>
+      );
+    } else {
+      return (
+        <a href={attributes.slug} target="_self" className={classNavbar}>
+          {attributes.menuTitle}
+        </a>
+      );
+    }
+  };
+
+  return (
+    <header>
+      <Container fixed>
+        <div className="items">
+          <div className="logo">
+            <a href={window.location.origin}>
+              <img src={logo} alt="" />
+            </a>
+          </div>
+
+          <ul>
+            {pages.map(({ attributes, id }) => (
+              <li key={id}>{getLinkNavbar(attributes, false)}</li>
+            ))}
+          </ul>
+
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer('right', true)}
+          >
+            <Menu />
+          </IconButton>
+        </div>
+      </Container>
+
+      <Drawer
+        anchor="right"
+        open={state['right']}
+        onClose={toggleDrawer('right', false)}
+      >
+        <Close
+          className="responsive-menu-close"
+          onClick={toggleDrawer('right', false)}
+        />
+
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer('right', false)}
+          onKeyDown={toggleDrawer('right', false)}
+        >
+          <List class="list-responsive-navbar">
+            {pages.map(({ attributes, id }) => (
+              <ListItem key={id} disablePadding>
+                {getLinkNavbar(attributes, true)}
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
+    </header>
+  );
+};
+
+export default Header;
