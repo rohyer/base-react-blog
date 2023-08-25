c58b1899b0ba1084b33660141cfcc530cccb7ee3
Rohyer <guilhermerl.dev@gmail.com>
2023-08-25 18:03:33 -0300

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import './App.css';
 import Header from './Home/Header/Header';
 import Footer from './Home/Footer/Footer';
 import Banner from './Home/Banner/Banner';
+import About from './Home/About/About';
+import AltAbout from './Home/AltAbout/AltAbout';
+import Services from './Home/Services/Services';
 
 function App() {
   return (
     <div>
       <Header />
       <Banner />
+      <About />
+      <Services />
+      <AltAbout />
       <Footer />
     </div>
   );
