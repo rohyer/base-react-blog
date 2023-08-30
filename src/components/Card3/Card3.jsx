1b515db17171f82860b0d0529de0887c3804e26b
Rohyer <guilhermerl.dev@gmail.com>
2023-08-29 23:38:39 -0300

    develop cards3 component

---
+++ src/components/Card3/Card3.jsx
@@ -0,0 +1,33 @@
+import './Card3.css';
+import React from 'react';
+
+const Card3 = ({ data, api }) => {
+  
+  const getPostCard = (data) => {
+    return (
+      <img src={`${import.meta.env.VITE_APP_API_URL}${data.attributes.cardImage.data.attributes.url}`} alt="Imagem" />
+      )
+    }
+    
+  const getPostLink = (data) => {
+    const content = data.attributes.innerContent;
+
+    if (content) {
+      return (
+        <a key={data.id} href={`${window.location.origin}/${api}/${data.attributes.slug}`} target='_self' className="post">
+          {getPostCard(data)}
+        </a>
+      )
+    } else {
+      return (
+        <div key={data.id} className="post">
+          {getPostCard(data)}
+        </div>
+      )
+    }
+  }
+
+  return getPostLink(data);
+}
+
+export default Card3;
