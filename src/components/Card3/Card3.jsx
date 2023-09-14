faeb755d207e5f7afe9b4e5b15ed67af65e1b661
Rohyer <guilhermerl.dev@gmail.com>
2023-09-14 00:26:40 -0300

    fix card3 component errors

--- src/components/Card3/Card3.jsx
+++ src/components/Card3/Card3.jsx
@@ -2,32 +2,41 @@ import './Card3.css';
 import React from 'react';
 
 const Card3 = ({ data, api }) => {
-  
   const getPostCard = (data) => {
     return (
-      <img src={`${import.meta.env.VITE_APP_API_URL}${data.attributes.cardImage.data.attributes.url}`} alt="Imagem" />
-      )
-    }
-    
+      <img
+        src={`${import.meta.env.VITE_APP_API_URL}${
+          data.attributes.cardImage.data.attributes.url
+        }`}
+        alt="Imagem"
+      />
+    );
+  };
+
   const getPostLink = (data) => {
     const content = data.attributes.innerContent;
 
     if (content) {
       return (
-        <a key={data.id} href={`${window.location.origin}/${api}/${data.attributes.slug}`} target='_self' className="post">
+        <a
+          key={data.id}
+          href={`${window.location.origin}/${api}/${data.attributes.slug}`}
+          target="_self"
+          className="post-partner"
+        >
           {getPostCard(data)}
         </a>
-      )
+      );
     } else {
       return (
-        <div key={data.id} className="post">
+        <div key={data.id} className="post-partner">
           {getPostCard(data)}
         </div>
-      )
+      );
     }
-  }
+  };
 
   return getPostLink(data);
-}
+};
 
 export default Card3;
