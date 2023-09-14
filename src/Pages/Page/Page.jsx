4573843747b2afc38139f8e3c8cb3be429936c94
Rohyer <guilhermerl.dev@gmail.com>
2023-09-14 00:27:39 -0300

    fix page and postspage components errors

--- src/Pages/Page/Page.jsx
+++ src/Pages/Page/Page.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Container, Button } from '@mui/material';
 // import { useNavigate } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
-
+import { useNavigate, useParams } from 'react-router-dom';
 
 const headers = {
-  'Authorization': 'Bearer ' + import.meta.env.VITE_APP_API_TOKEN
-}
+  Authorization: 'Bearer ' + import.meta.env.VITE_APP_API_TOKEN,
+};
 
 const Page = ({ id }) => {
   const [page, setPage] = React.useState({});
@@ -16,38 +16,49 @@ const Page = ({ id }) => {
 
   React.useEffect(() => {
     const fetchData = async () => {
-      const data = await fetch(`http://localhost:1337/api/paginas/${id}?populate=*`, {
-        headers
-      });
+      const data = await fetch(
+        `http://localhost:1337/api/paginas/${id}?populate=*`,
+        {
+          headers,
+        },
+      );
       const res = await data.json();
       setPage(res.data.attributes);
       setImage(res.data.attributes.innerImage.data.attributes.url);
-    }
+    };
 
     fetchData();
 
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }, [id]);
 
   return (
-    
     <div className="page">
       <Container fixed>
         <h1 class="inner-title--responsive">{page.innerTitle}</h1>
 
-        <img className="inner-img" src={`${import.menta.env.VITE_APP_API_URL}${image}`} alt="Imagem" />
+        <img
+          className="inner-img"
+          src={`${import.meta.env.VITE_APP_API_URL}${image}`}
+          alt="Imagem"
+        />
         <div className="inner-text">
           <h1 class="inner-title--desktop">{page.innerTitle}</h1>
-          <ReactMarkdown>
-            {page.innerContent}
-          </ReactMarkdown>
+          <ReactMarkdown>{page.innerContent}</ReactMarkdown>
         </div>
       </Container>
       <Container fixed>
-        <Button className="back-btn" variant="contained" color="inherit" onClick={() => navigate(-1)}>Voltar</Button>
+        <Button
+          className="back-btn"
+          variant="contained"
+          color="inherit"
+          onClick={() => navigate(-1)}
+        >
+          Voltar
+        </Button>
       </Container>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
