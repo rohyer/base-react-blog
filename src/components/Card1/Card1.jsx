0f3ed63618914103d3546a6bbba7cc3dd7651f4e
Rohyer <guilhermerl.dev@gmail.com>
2023-09-14 00:26:12 -0300

    develop card1 component

---
+++ src/components/Card1/Card1.jsx
@@ -0,0 +1,48 @@
+import './Card1.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Card1 = ({ data, api }) => {
+  const getPostCard = (data) => {
+    return (
+      <>
+        <img
+          src={`${import.meta.env.VITE_APP_API_URL}${
+            data.attributes.cardImage.data.attributes.url
+          }`}
+          alt="Imagem"
+        />
+        <div className="post-content">
+          <p>{data.attributes.cardTitle}</p>
+        </div>
+      </>
+    );
+  };
+
+  const getPostLink = (data) => {
+    const slug = data.attributes.slug;
+    const content = data.attributes.innerContent;
+
+    if (content) {
+      return (
+        <Link
+          key={data.id}
+          to={`${window.location.origin}/${api}/${data.attributes.slug}`}
+          className="post-news"
+        >
+          {getPostCard(data)}
+        </Link>
+      );
+    } else {
+      return (
+        <div key={data.id} className="post-news">
+          {getPostCard(data)}
+        </div>
+      );
+    }
+  };
+
+  return getPostLink(data);
+};
+
+export default Card1;
