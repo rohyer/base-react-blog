7c5e828c0bf283487fbbadfa603a22b37864c508
Rohyer <guilhermerl.dev@gmail.com>
2023-09-14 00:26:00 -0300

    add slug prop to route

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="quem-somos" element={<Page id={1} />} />
-        <Route path="noticias" element={<PostsPage id={2} />} />
+        <Route path="noticias" element={<PostsPage id={2} slug="noticias" />} />
         <Route path="cta" element={<Page id={3} />} />
-        <Route path="servicos" element={<PostsPage id={4} />} />
-        <Route path="parceiros" element={<PostsPage id={5} />} />
+        <Route path="servicos" element={<PostsPage id={4} slug="servicos" />} />
+        <Route
+          path="parceiros"
+          element={<PostsPage id={5} slug="parceiros" />}
+        />
       </Routes>
 
       <Footer />
