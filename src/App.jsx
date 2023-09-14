15300a49c9084a03c40f6e12abf8077dab17023e
Rohyer <guilhermerl.dev@gmail.com>
2023-09-13 23:55:05 -0300

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Header from './Home/Header/Header';
 import Footer from './Home/Footer/Footer';
 import Home from './Home/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Banner from './Home/Banner/Banner';
+import Page from './Pages/Page/Page';
+import PostsPage from './Pages/PostsPage/PostsPage';
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="quem-somos" element={<Page id={1} />} />
+        <Route path="noticias" element={<PostsPage id={2} />} />
+        <Route path="cta" element={<Page id={3} />} />
+        <Route path="servios" element={<PostsPage id={4} />} />
+        <Route path="parceiros" element={<PostsPage id={5} />} />
       </Routes>
 
       <Footer />
