3e0582edc8be19124a44d9e36e53e82296dcdd12
Rohyer <guilhermerl.dev@gmail.com>
2023-09-20 23:11:29 -0300

    Change import files and add posts prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import Header from './Components/Home/Header/Header';
 import Footer from './Components/Home/Footer/Footer';
 import Home from './Components/Home/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Page from './Components/Pages/Page/Page';
-import PostsPage from './Components/Pages/PostsPage/PostsPage';
-import Post from './Components/Pages/Post/Post';
-import PageContact from './Components/Pages/PageContact/PageContact';
+import Page from './Components/Page/Page';
+import Post from './Components/Post/Post';
+import PageContact from './Components/PageContact/PageContact';
 
 function App() {
   return (
@@ -15,27 +14,34 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="quem-somos" element={<Page id={1} />} />
-        <Route path="noticias" element={<PostsPage id={2} slug="noticias" />} />
+        <Route path="quem-somos" element={<Page id={1} posts={false} />} />
+        <Route
+          path="noticias"
+          element={<Page id={2} slug="noticias" posts={true} />}
+        />
+        <Route path="cta" element={<Page id={3} />} posts={false} />
+        <Route
+          path="servicos"
+          element={<Page id={4} slug="servicos" posts={true} />}
+        />
+        <Route
+          path="parceiros"
+          element={<Page id={5} slug="parceiros" posts={true} />}
+        />
+        <Route path="contato" element={<PageContact id={6} slug="contato" />} />
+
         <Route
           path="noticias/:slug"
           element={<Post collectionType="noticias" />}
         />
-        <Route path="cta" element={<Page id={3} />} />
-        <Route path="servicos" element={<PostsPage id={4} slug="servicos" />} />
         <Route
           path="servicos/:slug"
           element={<Post collectionType="servicos" />}
         />
         <Route
-          path="parceiros"
-          element={<PostsPage id={5} slug="parceiros" />}
-        />
-        <Route
           path="parceiros/:slug"
           element={<Post collectionType="parceiros" />}
         />
-        <Route path="contato" element={<PageContact id={6} slug="contato" />} />
       </Routes>
 
       <Footer />
