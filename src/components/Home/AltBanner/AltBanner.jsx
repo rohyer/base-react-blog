11e09a8b3839697df815ebcc44c098fccbe417d2
Rohyer <guilhermerl.dev@gmail.com>
2023-09-25 22:28:07 -0300

    Develop AltBanner

--- src/components/Home/AltBanner/AltBanner.jsx
+++ src/components/Home/AltBanner/AltBanner.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Container } from '@mui/material';
 import { Navigation, Pagination, Autoplay, Parallax } from 'swiper/modules';
-import './AltBanner.css';
-
-// import 'swiper/css/swiper.css';
-// import Swiper from 'react-id-swiper';
+import styles from './AltBanner.module.css';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -19,30 +16,31 @@ const headers = {
   Authorization: 'Bearer ' + import.meta.env.VITE_APP_API_TOKEN,
 };
 
-const HeroSliderConfigs = {
-  containerClass: 'swiper-container hero-slider',
-  parallax: true,
-  centeredSlides: true,
-  speed: 500,
-  spaceBetween: 0,
-  effect: 'slide',
-};
-
 const AltBanner = () => {
   const [banners, setBanners] = React.useState([]);
-  const [parallaxSwiper, setParallaxSwiper] = React.useState(null);
-  const parallaxAmount = parallaxSwiper ? parallaxSwiper.width * 0.95 : 0;
-  const parallaxOpacity = 0.5;
+  const [bannerImage, setBannerImage] = React.useState({});
 
   React.useEffect(() => {
     const fetchData = async () => {
-      const data = await fetch('http://localhost:1337/api/banners?populate=*', {
+      const data = await fetch('http://localhost:1337/api/banners', {
         headers,
       });
       const res = await data.json();
       setBanners(res.data);
     };
+
+    const fetchImage = async () => {
+      const response = await fetch(
+        'http://localhost:1337/api/informacao?populate=*',
+        {
+          headers,
+        },
+      );
+      const json = await response.json();
+      setBannerImage(json.data.attributes.bannerImage.data);
+    };
     fetchData();
+    fetchImage();
   }, []);
 
   const getLink = (attributes) => {
@@ -68,7 +66,7 @@ const AltBanner = () => {
   };
 
   return (
-    <section className="banner">
+    <section className={styles.banner}>
       <Container maxWidth="xl" style={{ padding: 0 }}>
         <Swiper
           modules={[Navigation, Pagination, Autoplay, Parallax]}
@@ -81,21 +79,24 @@ const AltBanner = () => {
           spaceBetween={0}
           slidesPerView={1}
           // onSlideChange={() => console.log(watchSlidesProgress)}
-          onSwiper={(swiper) => console.log(swiper.width)}
+          // onSwiper={(swiper) => console.log(swiper.width)}
         >
-          <div
-            data-swiper-parallax-x="20%"
-            style={{
-              backgroundImage: `url(${import.meta.env.VITE_APP_API_URL}${
-                attributes.homeImage.data.attributes.url
-              })`,
-            }}
-            className="image"
-          ></div>
+          {bannerImage.attributes && (
+            <div
+              data-swiper-parallax="-23%"
+              style={{
+                backgroundImage: `url(${import.meta.env.VITE_APP_API_URL}${
+                  bannerImage.attributes.url
+                })`,
+              }}
+              className={styles.image}
+            ></div>
+          )}
+
           {banners &&
             banners.map(({ attributes, id }) => (
               <SwiperSlide key={id}>
-                <div className="content">
+                <div className={styles.content}>
                   <h2>{attributes.homeTitle}</h2>
                   <p>{attributes.homeContent}</p>
                   {getLink(attributes)}
